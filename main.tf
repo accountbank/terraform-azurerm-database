e3eba56b4fc13099d85da24399859d527f5811c5
Cristiano Ledo <cristiano.ledo@live.com>
2023-05-11 06:09:01 -0300

    Change server name to variable and update resource to "azurerm_mssql_database"

--- main.tf
+++ main.tf
@@ -5,14 +5,9 @@
 #   tenant_id       = "REPLACE-WITH-YOUR-TENANT-ID"
 # }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
-}
-
-resource "azurerm_sql_database" "db" {
+resource "azurerm_mssql_database" "db" {
   name                             = "${var.db_name}"
-  resource_group_name              = "${azurerm_resource_group.rg.name}"
+  resource_group_name              = "${var.resource_group_name}"
   location                         = "${var.location}"
   edition                          = "${var.db_edition}"
   collation                        = "${var.collation}"
@@ -23,8 +18,8 @@ resource "azurerm_sql_database" "db" {
 }
 
 resource "azurerm_sql_server" "server" {
-  name                         = "${var.db_name}-sqlsvr"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  name                         = "${var.server_name}-sqlsvr"
+  resource_group_name          = "${var.resource_group_name}"
   location                     = "${var.location}"
   version                      = "${var.server_version}"
   administrator_login          = "${var.sql_admin_username}"
@@ -34,7 +29,7 @@ resource "azurerm_sql_server" "server" {
 
 resource "azurerm_sql_firewall_rule" "fw" {
   name                = "${var.db_name}-fwrules"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = "${var.resource_group_name}"
   server_name         = "${azurerm_sql_server.server.name}"
   start_ip_address    = "${var.start_ip_address}"
   end_ip_address      = "${var.end_ip_address}"
